67f6a7377ed00202017582a5265d3147ce0f2112
ParsaYadollahi <parsa.yadollahi@mail.mcgill.ca>
2020-10-23 16:26:24 -0400

    Going to try for YAU

--- A2/Support/YAUThreads/YAUThreads.c
+++ A2/Support/YAUThreads/YAUThreads.c
@@ -1,86 +1,78 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
-#include <stdlib.h>                                                                              
+#include <stdlib.h>
 #include "YAUThreads.h"
 
 threaddesc threadarr[MAX_THREADS];
 int numthreads, curthread;
 ucontext_t parent;
 
-
 void initYAUThreads()
 {
-	numthreads = 0;
-	curthread = 0;
+  numthreads = 0;
+  curthread = 0;
 }
 
-
-int YAUSpawn( void (threadfunc)() )
+int YAUSpawn(void(threadfunc)())
 {
-	threaddesc *tdescptr;
+  threaddesc *tdescptr;
 
-	if (numthreads >= 32) 
-	{
-		printf("FATAL: Maximum thread limit reached... creation failed! \n");
-		return -1;
-	}
+  if (numthreads >= 32)
+  {
+    printf("FATAL: Maximum thread limit reached... creation failed! \n");
+    return -1;
+  }
 
-	tdescptr = &(threadarr[numthreads]);
-	getcontext(&(tdescptr->threadcontext));
-	tdescptr->threadid = numthreads;
-	tdescptr->threadstack = (char *)malloc(THREAD_STACK_SIZE);
-	tdescptr->threadcontext.uc_stack.ss_sp = tdescptr->threadstack;
-	tdescptr->threadcontext.uc_stack.ss_size = THREAD_STACK_SIZE;
-	tdescptr->threadcontext.uc_link = 0;
-	tdescptr->threadcontext.uc_stack.ss_flags = 0;
-	tdescptr->threadfunc = threadfunc;
+  tdescptr = &(threadarr[numthreads]);
+  getcontext(&(tdescptr->threadcontext));
+  tdescptr->threadid = numthreads;
+  tdescptr->threadstack = (char *)malloc(THREAD_STACK_SIZE);
+  tdescptr->threadcontext.uc_stack.ss_sp = tdescptr->threadstack;
+  tdescptr->threadcontext.uc_stack.ss_size = THREAD_STACK_SIZE;
+  tdescptr->threadcontext.uc_link = 0;
+  tdescptr->threadcontext.uc_stack.ss_flags = 0;
+  tdescptr->threadfunc = threadfunc;
 
-	makecontext(&(tdescptr->threadcontext), threadfunc, 1, tdescptr);
+  makecontext(&(tdescptr->threadcontext), threadfunc, 1, tdescptr);
 
-	numthreads++;
+  numthreads++;
 
-	return 0;
+  return 0;
 }
 
-
-void handle_timerexpiry() 
+void handle_timerexpiry()
 {
-        struct sigaction handler;
-	int nxtthread, curthreadsave;
-	
-        handler.sa_handler = handle_timerexpiry;
-        sigaction(SIGALRM, &handler, NULL);
-        alarm(RR_QUANTUM);
+  struct sigaction handler;
+  int nxtthread, curthreadsave;
 
-	nxtthread = (curthread +1) % numthreads;
+  handler.sa_handler = handle_timerexpiry;
+  sigaction(SIGALRM, &handler, NULL);
+  alarm(RR_QUANTUM);
 
-	curthreadsave = curthread;
-	curthread = nxtthread;
-	swapcontext(&(threadarr[curthreadsave].threadcontext),
-		    &(threadarr[nxtthread].threadcontext));
+  nxtthread = (curthread + 1) % numthreads;
+
+  curthreadsave = curthread;
+  curthread = nxtthread;
+  swapcontext(&(threadarr[curthreadsave].threadcontext),
+              &(threadarr[nxtthread].threadcontext));
 }
- 
 
 void startYAUThreads(int sched)
 {
-	struct sigaction handler;
+  struct sigaction handler;
 
-	if (sched == RR && numthreads > 0)
-	{
-		handler.sa_handler = handle_timerexpiry;
-		sigaction(SIGALRM, &handler, NULL);
-		alarm(RR_QUANTUM);
+  if (sched == RR && numthreads > 0)
+  {
+    handler.sa_handler = handle_timerexpiry;
+    sigaction(SIGALRM, &handler, NULL);
+    alarm(RR_QUANTUM);
 
-		swapcontext(&parent, &(threadarr[curthread].threadcontext));
-	}	
+    swapcontext(&parent, &(threadarr[curthread].threadcontext));
+  }
 }
-
 
 int getYAUThreadid(threaddesc *th)
 {
-	return th->threadid;
+  return th->threadid;
 }
-
-		
-	
--- A2/Support/sut.c
+++ A2/Support/sut.c
@@ -16,7 +16,7 @@
 #include <time.h>
 
 typedef void (*sut_task_f)();
-static ucontext_t t1, t2, m;
+static ucontext_t t1, m;
 
 void f1()
 {
@@ -28,20 +28,52 @@ void f1()
   }
 }
 
-// void f2()
-// {
-//   while (true)
-//   {
-//     usleep(1000 * 1000);
-//     printf("Hello world!, this is SUT-Two \n");
-//     swapcontext(&t2, &m);
-//   }
-// }
+void *c_exec_ftn(void *args)
+{
+  pthread_mutex_t *m = args;
+  while (true)
+  {
+    pthread_mutex_lock(m);
+    printf("1 C-exec thread\n");
+    usleep(1000 * 1000);
+    printf("1 CEXEC\n");
+    pthread_mutex_unlock(m);
+    usleep(1000 * 1000);
+  }
+}
+
+void *i_exec_ftn(void *args)
+{
+  pthread_mutex_t *m = args;
+  while (true)
+  {
+    pthread_mutex_lock(m);
+    printf("2 I-exec thread\n");
+    usleep(1000 * 1000);
+    printf("2 IEXEC\n");
+    pthread_mutex_unlock(m);
+    usleep(1000 * 1000);
+  }
+}
+
+int sut_init()
+{
+  // Initialize kernel-level threads
+  pthread_t c_exec_thread;
+  pthread_t i_exec_thread;
+
+  // Initialize pthread mutex - lock
+  pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+
+  pthread_create(&c_exec_thread, NULL, c_exec_ftn, &m);
+  pthread_create(&i_exec_thread, NULL, i_exec_ftn, &m);
+  pthread_join(c_exec_thread, NULL);
+  pthread_join(i_exec_thread, NULL);
+}
 
 int main()
 {
   char f1stack[16 * 1024];
-  char f2stack[16 * 1024];
 
   getcontext(&t1);
   t1.uc_stack.ss_sp = f1stack;
@@ -49,35 +81,10 @@ int main()
   t1.uc_link = &m;
   makecontext(&t1, f1, 0);
 
-  // getcontext(&t2);
-  // t2.uc_stack.ss_sp = f2stack;
-  // t2.uc_stack.ss_size = sizeof(f2stack);
-  // t2.uc_link = &m;
-  // makecontext(&t2, f2, 0);
-
   while (true)
   {
     swapcontext(&m, &t1);
-    // swapcontext(&m, &t2);
   }
 
   return 0;
 }
-
-void sut_init()
-{
-  // Initialize kernel-level threads
-  pthread_t c_exec;
-  pthread_t i_exec;
-
-  pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
-
-  pthread_create(&c_exec, NULL, sut_init, NULL);
-  pthread_create(&i_exec, NULL, sut_init, NULL);
-  pthread_join(c_exec, NULL);
-}
-
-// bool sut_create(sut_task_f fn)
-// {
-//   return true;
-// }
