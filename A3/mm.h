77bcaf361f0e7613f5bff0964dd07212c8ff19b9
ParsaYadollahi <parsa.yadollahi@mail.mcgill.ca>
2020-11-07 19:59:16 -0500

    Started code for A3

--- A3/mm.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void *sma_malloc(int size);
-void sma_free(void *ptr);
-void sma_mallopt(int policy);
-void sma_mallinfo();
-void *sma_realloc(void *ptr, int size);
-
-int sma_init(void);
